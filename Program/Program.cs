e7af25e3e680627781a19ded3ee7e24565594ec2
EmilLindquist <emil789u@mail.hansenberg.dk>
2023-12-05 12:47:06 +0100

    Edited the Project
    learned about different data types

--- Program/Program.cs
+++ Program/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int x = 4;
+int y = 5;
+int z = x + y;
+int d = x + y - z * x / z % y;
+
+string str = "Result of d is = ";
+
+Console.WriteLine("x = " + x + " and y = " + y + " and x + y = " + z);
+Console.WriteLine(str + d);
