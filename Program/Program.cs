cc246ae99b1dba804e23fa9db5d473d28800218c
EmilLindquist <emil789u@mail.hansenberg.dk>
2023-12-12 11:55:52 +0100

    We have learned more
    about bool and more about float

--- Program/Program.cs
+++ Program/Program.cs
@@ -1,4 +1,5 @@
-﻿int x = 4;
+﻿/*
+int x = 4;
 int y = 5;
 int z = x + y;
 int d = x + y - z * x / z % y;
@@ -7,3 +8,19 @@ string str = "Result of d is = ";
 
 Console.WriteLine("x = " + x + " and y = " + y + " and x + y = " + z);
 Console.WriteLine(str + d);
+*/
+
+bool isFour = true;
+int x = 4;
+
+Console.WriteLine("x is 4: " + isFour);
+
+string gamer = "gamer \ngamer";
+Console.WriteLine(gamer);
+
+Console.WriteLine("give me an input: ");
+double d = checked;
+{
+    ConvertToDouble.(Console.Readline());
+}
+Console.WriteLine(d);
